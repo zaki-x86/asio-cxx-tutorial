fe6067e5765c445243c336a8ad64656b364afaed
zaki-x86 <mamadovzicx@gmail.com>
2024-02-26 08:07:07 +0200

    Added illustrative comments to client_sync example

--- networking/client_sync.cpp
+++ networking/client_sync.cpp
@@ -11,24 +11,27 @@ int main(int argc, char const *argv[])
     // Create context
     // Resolve endpoints
     // Create socket
-    // Connect socket to endpoints
+    // Connect socket to endpoint
     asio::io_context io_context;
     asio::error_code err;
     tcp::resolver::results_type endpoints = tcp::resolver(io_context).resolve("example.com", "http");
     tcp::socket socket(io_context);
     auto endpoint = asio::connect(socket, endpoints, err);
 
+    // Step. 2
+    // Check if socket is open and connection is established
     if (err) {
         std::cerr << "Error: " << err.message() << "\n";
         return 1;
     }
     std::cout << "Connected to " << endpoint.address() << ":" << endpoint.port() << "\n\n";
-    
+
     if (!socket.is_open()) {
         std::cerr << "Error: Socket is not open" << "\n";
         return 1;
     }
 
+    // Step. 3
     // Send HTTP request
     std::string request = "GET / HTTP/1.1\r\n"
                         "Host: example.com\r\n"
@@ -40,6 +43,8 @@ int main(int argc, char const *argv[])
     }
     std::cout << "Sent HTTP request: " << request << "\n\n";
     
+    // Step. 4
+    // Wait for data to be received
     socket.wait(tcp::socket::wait_read, err);
     if (err) {
         std::cerr << "Error: " << err.message() << "\n";
@@ -50,6 +55,9 @@ int main(int argc, char const *argv[])
     // it just tells you there's something
     // so you can't rely on it for buffer memory allocation
     size_t bytes = socket.available();
+
+    // Step. 5
+    // Read data from socket and output to console
     if(bytes > 0) {
         std::cout << "Bytes available: " << bytes << "\n\n";
         // Read response from socket and output to console
